588551fcfa92191d0444df81bb2cff814b832d10
Sam <sam9elliss@gmail.com>
2025-02-19 13:33:24 -0700

    added an endpoint

--- api/data.py
+++ api/data.py
@@ -4,7 +4,15 @@ api = Namespace('data', description='Data Stats')
 
 
 @api.route('/total-usage/<string:date_range>')
-# Data = relevent data for date range
+# Data = relevant data for date range
 class TotalUsage(Resource):
     def get(self, date_range):
         return {'TotalUsage': Data}
+    
+# max energy cap
+@api.route('max-energy/<string:date_range>')
+# Data = relevant data for date range
+class MaxEnergy(Resource):
+    def get(self, date_range):
+        return {'MaxEnergy': Data}
+    
