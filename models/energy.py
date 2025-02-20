6ba23d175948d1ae21dfe76d4031ef8660c0ed5f
Mindcool25 <thezachm24@gmail.com>
2025-02-19 21:50:49 -0700

    Should be good enough for now

--- models/energy.py
+++ models/energy.py
@@ -25,34 +25,39 @@ class DataEntry(Model):
     class Meta:
         database = db
 
+# Very hacked together I should fix this - Zach
+# Also fix my timestamp shenanagans
+def main():
+    db.connect()
+    db.create_tables([Device, DataEntry])
+    with open('devices.csv', newline='') as devices:
+        reader = csv.reader(devices)
+        for row in reader:
+            # ID = 0
+            # display name = 1
+            # equpment type = 2
+            # ip = 3
+            print(f"ID: {row[0]}")
+            print(f"Display Name: {row[1]}")
+            print(f"Equp. Type = {row[2]}")
+            tempDev = Device(device_id=row[0], display_name=row[1], equipment_type=row[2])
+            tempDev.save()
 
-db.connect()
-db.create_tables([Device, DataEntry])
-with open('devices.csv', newline='') as devices:
-    reader = csv.reader(devices)
-    for row in reader:
-        # ID = 0
-        # display name = 1
-        # equpment type = 2
-        # ip = 3
-        print(f"ID: {row[0]}")
-        print(f"Display Name: {row[1]}")
-        print(f"Equp. Type = {row[2]}")
-        tempDev = Device(device_id=row[0], display_name=row[1], equipment_type=row[2])
-        tempDev.save()
+    with open('entries.csv', newline='') as entries:
+        reader = csv.reader(entries)
+        for row in reader:
+            # timestamp = 0
+            # device = 1
+            # voltage = 2
+            # current = 3
+            # power = 4
+            # state = 6
+            # fault = 7
+            # entry = 8
+            entry_device = Device.select().where(Device.device_id == row[1]).get()
+            tempEntry = DataEntry(timestamp=row[0], device=entry_device, voltage=row[2], current=row[3], power=row[4], state=row[6], fault=row[7], entry=row[8])
+            tempEntry.save()
+            print('.', end='')
 
-with open('entries.csv', newline='') as entries:
-    reader = csv.reader(entries)
-    for row in reader:
-        # timestamp = 0
-        # device = 1
-        # voltage = 2
-        # current = 3
-        # power = 4
-        # state = 6
-        # fault = 7
-        # entry = 8
-        entry_device = Device.select().where(Device.device_id == row[1]).get()
-        tempEntry = DataEntry(timestamp=row[0], device=entry_device, voltage=row[2], current=row[3], power=row[4], state=row[6], fault=row[7], entry=row[8])
-        tempEntry.save()
-        print('.', end='')
+if __name__ == "__main__":
+    main()
