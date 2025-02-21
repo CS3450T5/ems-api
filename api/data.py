f304d2e5ad8c377cd238888ad56e9566afa840b3
Peter Shakespear <peter.shakespear@gmail.com>
2025-02-20 19:37:34 -0700

    api updates

--- api/data.py
+++ api/data.py
@@ -6,5 +6,5 @@ api = Namespace('data', description='Data Stats')
 @api.route('/total-usage/<string:date_range>')
 # Data = relevent data for date range
 class TotalUsage(Resource):
-    def get(self, date_range):
+    def get(self, Date: date_range):
         return {'TotalUsage': Data}
