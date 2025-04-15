af6071c856ae51d6d9e6988c4c3612751af27d18
mindcool24 <thezachm24@gmail.com>
2025-04-15 17:36:37 -0600

    hopefully this works

--- functions/device_total_function.py
+++ functions/device_total_function.py
@@ -6,9 +6,9 @@ def get_device_total_usage(device_name: str):
     power_total = 0
     for device in Device.select().where(Device.device_id == device_name):
         display_name = device.display_name
-    # for entry in DataEntry.select().join(Device).where(Device.device_id == device_name):
-        # voltage_total += entry.voltage
-        # current_total += entry.current
-        # power_total += entry.power
+    for entry in DataEntry.select().join(Device).where(Device.device_id == device_name):
+         voltage_total += entry.voltage
+         current_total += entry.current
+         power_total += entry.power
 
     return (display_name, voltage_total, current_total, power_total)
