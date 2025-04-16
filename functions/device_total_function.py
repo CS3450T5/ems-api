021186e1bc840372c7f5c60cb300e5ca60402f9d
mindcool24 <thezachm24@gmail.com>
2025-04-15 18:07:56 -0600

    Sped up device total usage function to use db instead of python for adding things

--- functions/device_total_function.py
+++ functions/device_total_function.py
@@ -1,4 +1,5 @@
 from models import Device, DataEntry
+from peewee import fn
 
 def get_device_total_usage(device_name: str):
     voltage_total = 0
@@ -6,9 +7,8 @@ def get_device_total_usage(device_name: str):
     power_total = 0
     for device in Device.select().where(Device.device_id == device_name):
         display_name = device.display_name
-    for entry in DataEntry.select().join(Device).where(Device.device_id == device_name):
-         voltage_total += entry.voltage
-         current_total += entry.current
-         power_total += entry.power
+    voltage_total = DataEntry.select(fn.SUM(DataEntry.voltage)).join(Device).where(Device.device_id == device_name).scalar()
+    current_total = DataEntry.select(fn.SUM(DataEntry.current)).join(Device).where(Device.device_id == device_name).scalar()
+    power_total = DataEntry.select(fn.SUM(DataEntry.power)).join(Device).where(Device.device_id == device_name).scalar()
 
     return (display_name, voltage_total, current_total, power_total)
